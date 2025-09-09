b13da3d4e12ec717c66171abc404e0013259b232
JoseAlci <jose.e.alcivar.c@outlook.com>
2025-09-08 20:38:25 -0500

    ver01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,122 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
   title = 'evertec_help_system';
+
+
+  result: string = "";
+
+
+  actions_list = [
+    { value: "parse_highlighting_spaces", label: "Parsear realzando espacios"},
+    { value: "parse_no_highlighting_spaces", label: "Parsear sin realzar espacios"}
+  ]
+
+
+  process_tokens(value_select: string, value_token: string) {
+
+      if (value_select === "parse_highlighting_spaces") {
+        this.parse_highlighting_spaces(value_token.replace(/(\r\n|\n|\r)/g, ''));
+      }
+
+      if (value_select === "parse_no_highlighting_spaces") {
+        this.parse_no_highlighting_spaces(value_token.replace(/(\r\n|\n|\r)/g, ''));
+      }
+
+  }
+
+
+
+  //----------------------------------------FUNCTIONS----------------------------------------
+
+  parse_highlighting_spaces(original_token: string): void {
+
+    let current_parsed = "NOTA: En 'lista completa' y en 'token content', los signos '%' al comienzo y final, marcan el comienzo y final del mismo, NO forman parte del token\n\n";
+    
+    const field_length: string = original_token.split("&")[0];
+
+    const field_content: string = original_token.split("&")[1]
+
+    const tokens_length: string = field_content.split("!")[0];
+
+    const tokens_list: string[] = field_content.split("!").slice(1);
+
+
+    const lista_completa: string = `lista completa: %&${field_content}%`;
+
+    current_parsed = current_parsed + lista_completa + "\n\n\n";
+
+    for (let i = 0; i < tokens_list.length; i++) {
+
+      const token_content = tokens_list[i].slice(1);
+            
+      const token_id = token_content.slice(0,2);
+      const token_length = token_content.slice(2,7);
+      const token_inf = token_content.slice(8);
+
+      const register = `------------------------------------- TOKEN ${token_id}-------------------------------------
+${token_content}
+
+token id: ${token_id}
+token length: ${token_length}
+token content: %${token_inf.replaceAll(" ", "¦")}%`
+
+
+      current_parsed = current_parsed + register + "\n\n\n";
+
+    }
+
+    this.result = current_parsed;
+
+  }
+
+
+  parse_no_highlighting_spaces(original_token: string): void {
+
+    let current_parsed = "NOTA: En 'lista completa' y en 'token content', los signos '%' al comienzo y final, marcan el comienzo y final del mismo, NO forman parte del token\n\n";
+    
+    const field_length: string = original_token.split("&")[0];
+
+    const field_content: string = original_token.split("&")[1]
+
+    const tokens_length: string = field_content.split("!")[0];
+
+    const tokens_list: string[] = field_content.split("!").slice(1);
+
+    const lista_completa: string = `lista completa: %&${field_content}%`;
+
+    current_parsed = current_parsed + lista_completa + "\n\n\n";
+
+    for (let i = 0; i < tokens_list.length; i++) {
+
+      const token_content = tokens_list[i].slice(1);
+            
+      const token_id = token_content.slice(0,2);
+      const token_length = token_content.slice(2,7);
+      const token_inf = token_content.slice(8);
+
+      const register = `------------------------------------- TOKEN ${token_id}-------------------------------------
+${token_content}
+
+token id: ${token_id}
+token length: ${token_length}
+token content: %${token_inf}%`
+
+
+      current_parsed = current_parsed + register + "\n\n\n";
+
+    }
+
+    this.result = current_parsed;
+
+  }
+
 }
