70a890414cd7f83e61538f600d490285f50b2780
José Alcivar C. <jose.e.alcivar.c@outlook.com>
2025-09-23 20:13:31 -0500

    ver07

--- src/app/pages/tokens-format/tokens-format.component.ts
+++ src/app/pages/tokens-format/tokens-format.component.ts
@@ -117,4 +117,18 @@ token content: %${token_inf}%`
 
   }
 
+  copyToClipboard(text: string) {
+
+    navigator.clipboard.writeText(text).then(() => {
+
+      alert('Texto copiado al portapapeles');
+
+    }).catch(err => {
+
+      console.error('Error al copiar: ', err);
+
+    });
+
+  }
+
 }
